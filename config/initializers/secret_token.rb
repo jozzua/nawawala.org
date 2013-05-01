a4e56507cfedd75f3ce263b27551d0d62aa920cc
Elymar Apao <jozzua@gmail.com>
2013-05-01 11:07:47 +0800

    Initial Ruby on Rails Base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NawawalaOrg::Application.config.secret_token = '16892c64f8a0c14c72775d6716cb4fac82c79373b1bc3b776af89f77eb10437da8aef23716aa83f74c23ec1be8fe77c5cb0d716ffef3dc222c5caf6b5b9d4759'
